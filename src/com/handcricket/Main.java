e7c067dced2ce0da12cabfc70238e9a050a63340
Amal K K <kkamalkk1994@gmail.com>
2022-07-03 13:40:04 +0530

    input validations

--- src/com/handcricket/Main.java
+++ src/com/handcricket/Main.java
@@ -6,29 +6,46 @@ import java.util.Scanner;
 public class Main {
 	
 	public static void main(String args[]) {
+		
+		int userScore;
+		userScore = playInnings("Batting");
+		System.out.println("User Score:" + userScore);
+	}
+	
 
+	
+	public static int playInnings(String currentRound) {
+		
 		Scanner sc = new Scanner(System.in);
 		String input;
 		Random rand = new Random();
 		int score = 0;
 		int cpuInput;
+		int userInput;
 		System.out.println("Aarambikaangala??");
 		do {
 			System.out.println("Enter your input:");
-			input = sc.nextLine();
-			cpuInput = rand.nextInt(7);
-			System.out.println("User:" + input);
-			System.out.println("CPU:" + cpuInput);
+			input = sc.nextLine();		
 			try {
-				if(cpuInput == Integer.parseInt(input)) {
-					System.out.println("OUT!!!!");
-					input = "exit";
+				if (!(input.toLowerCase().equals("exit"))) {
+					cpuInput = rand.nextInt(7);
+					System.out.println("User:" + input);
+					System.out.println("CPU:" + cpuInput);
+					userInput = Integer.parseInt(input);
+					if (!(userInput > 0 && userInput < 7)) {
+						throw new Exception("Invalid input");
+					}
+					if(cpuInput == userInput) {
+						System.out.println("OUT!!!!");
+						input = "exit";
+					}
+					else {
+						score += userInput;
+					}
+					System.out.println("Current score:" + score);					
 				}
-				else {
-					score += Integer.parseInt(input);
-				}
-				System.out.println("Current score:" + score);
-			} catch(NumberFormatException e) {
+
+			} catch(Exception e) {
 				System.out.println(e);
 				input = "exit";
 			}
@@ -37,7 +54,7 @@ public class Main {
 		
 		System.out.println("Final Score:" + score);
 		System.out.println("Exiting....");
-		sc.close();
+		sc.close();			
+		return score;
 	}
-
 }

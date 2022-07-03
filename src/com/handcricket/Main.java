ee398b7a0498a813857f648f64de1d84f439777c
Amal K K <kkamalkk1994@gmail.com>
2022-07-03 13:03:33 +0530

    basic working logic

--- src/com/handcricket/Main.java
+++ src/com/handcricket/Main.java
@@ -1,21 +1,43 @@
 package com.handcricket;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 	
 	public static void main(String args[]) {
 
-		System.out.println("Aarambikaangala??");
 		Scanner sc = new Scanner(System.in);
 		String input;
-		System.out.println("Enter your input:");
+		Random rand = new Random();
+		int score = 0;
+		int cpuInput;
+		System.out.println("Aarambikaangala??");
 		do {
+			System.out.println("Enter your input:");
 			input = sc.nextLine();
-			System.out.println(input);
+			cpuInput = rand.nextInt(7);
+			System.out.println("User:" + input);
+			System.out.println("CPU:" + cpuInput);
+			try {
+				if(cpuInput == Integer.parseInt(input)) {
+					System.out.println("OUT!!!!");
+					input = "exit";
+				}
+				else {
+					score += Integer.parseInt(input);
+				}
+				System.out.println("Current score:" + score);
+			} catch(NumberFormatException e) {
+				System.out.println(e);
+				input = "exit";
+			}
+			
 		} while(!input.toLowerCase().equals("exit"));
 		
+		System.out.println("Final Score:" + score);
 		System.out.println("Exiting....");
+		sc.close();
 	}
 
 }
